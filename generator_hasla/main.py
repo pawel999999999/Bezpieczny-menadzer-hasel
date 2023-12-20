a604df5147950f74ed7fc6844fda06d91ab7e5f3
Wojciech Matraś <wojtek>
2023-12-20 19:05:25 +0100

    3 commit

--- generator_hasla/main.py
+++ generator_hasla/main.py
@@ -34,7 +34,7 @@ class MainWindow:
         randomGen = random.sample(
             randomUpper + randomSpecial + randomNum + random.choices(string.ascii_lowercase, k=16),
             k=int(self.length.get()))
-        with open('password.txt', 'a') as file:
+        with open('haslo.txt', 'a') as file:
             file.write(f"{''.join(randomGen)}\n")
 
     def adds(self):
