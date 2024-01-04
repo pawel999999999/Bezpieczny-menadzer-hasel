6c43253d95c8236137a06387e60ca64dde1c99be
Wojciech Matraś <wojtek>
2024-01-04 11:35:44 +0100

    6,1 commit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import customtkinter
-import hashlib
-import uuid
+import customtkinter #gui
+import hashlib #hashowanie
+import uuid #tworzenie sniegu
+import subprocess  #otworzenie innego programu py
+import sys
+import time
+import hmac
+import cryptography.hazmat
 
 customtkinter.set_appearance_mode("Dark")
 customtkinter.set_default_color_theme("dark-blue")
@@ -8,23 +13,55 @@ gui = customtkinter.CTk()
 gui.geometry("700x400")
 uzytkownik = ""
 haslo = ""
+sciezka = "hasla.txt"
+a=0
+tajnyKlucz = "Moj_tajny_klucz"
+tajnyKlucz2 = b'Nowt_tajny_klucz'
+textOdszyfrowany = " "
 
+def sprawdz(szukany):
+    with open(sciezka, 'r') as plik:
+        zawartosc = plik.read()
+        print(szukany)
+        if szukany in zawartosc:
+            return True
+        else:
+            return False
+def oszyfrowanie(file):
+    return 0
 
-def _hash(password):
-    print(hashlib.sha512(password.encode()))
-    return hashlib.sha512(password.encode())
+def hash(password):
+    hashed = hmac.new(tajnyKlucz.encode('utf-8'), password.encode('utf-8'), hashlib.sha256)
+    print(password.encode('utf-8'))
+    print(hashed.hexdigest())
+    return hashed.hexdigest()
 
 def login():
     state1 = button1.get()
+    nazwaUz = button1.get()
     state2 = button2.get()
     print("Sprawdzam dane:")
-    print(state1)
-    print(state2)
-    _hash(state2)
+    state1 = hmac.new(tajnyKlucz.encode('utf-8'), state1.encode('utf-8'), hashlib.sha256)
+    state2 = hmac.new(tajnyKlucz.encode('utf-8'), state2.encode('utf-8'), hashlib.sha256)
+    state=state1.hexdigest()+state2.hexdigest()
+    print(state)
+    if(sprawdz(state)):
+        try:
+            gui2 = subprocess.Popen(['python', "program3.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+            gui.destroy()
+            while gui2:
+                time.sleep(1)
+        except subprocess.CalledProcessError as e:
+            print('Błąd')
 
+def zajerestruj():
+   
+    try:
+        subprocess.Popen(['python', "program2.py" ])
+    except subprocess.CalledProcessError as e:
+        print('Błąd')
+    
 
-def button_event():
-   print("button pressed")
 
 
 frame = customtkinter.CTkFrame(master=gui)
@@ -44,7 +81,7 @@ button2.pack(pady=15, padx=10)
 
 button3 = customtkinter.CTkButton(master=gui, state="normal", text="zaloguj", command=login)
 button3.pack(pady=12, padx=10)
-button4 = customtkinter.CTkButton(master=gui, state="normal", text="zarejestruj", command=login)
+button4 = customtkinter.CTkButton(master=gui, state="normal", text="zarejestruj", command=zajerestruj)
 button4.pack(pady=12, padx=10)
 
 
